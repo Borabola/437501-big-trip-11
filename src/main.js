29be368d6af0b466843fcb6579d5e87d2adcf88e
Olya <osokolova1@gmail.com>
2020-04-05 16:43:06 +0300

    Отрисовка точек маршрута с учетом количества дней

--- src/main.js
+++ src/main.js
@@ -311,22 +311,35 @@ const createTripEventsItemTemplate = () => {
   );
 };
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
-const creatDayListTemplate = () => {
+const createTripDaysItemTemplate = (tripEventsList) => {
   return (
-    `<ul class="trip-days">
-            <li class="trip-days__item  day">
+    `<li class="trip-days__item  day">
               <div class="day__info">
                 <span class="day__counter">1</span>
                 <time class="day__date" datetime="2019-03-18">MAR 18</time>
               </div>
 
-              <ul class="trip-events__list"></ul>
-            </li>
-    </ul>`
+              <ul class="trip-events__list">${tripEventsList}</ul>`
+  );
+};
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const creatDayListTemplate = () => {
+  let dayContent = ``;
+
+  for (let i = 0; i < DAY_COUNT; i++) {
+    let pointsContent = ``;
+    for (let j = 0; j < POINT_COUNT; j++) {
+      pointsContent += createTripEventsItemTemplate();
+    }
+    dayContent += createTripDaysItemTemplate(pointsContent);
+  }
+
+  return (
+    `<ul class="trip-days">${dayContent}</ul>`
   );
 };
 
@@ -339,10 +352,7 @@ render(tripControlBlock.children[0], createTripControlsTemplate(), `afterEnd`);
 render(tripControlBlock, createTripFiltersTemplate(), `beforeend`);
 render(tripEventSection.children[0], createTripSortTemplate(), `afterEnd`);
 render(tripEventSection.children[1], createEventEditTemplate(), `afterEnd`);
-render(tripEventSection, creatDayListTemplate(), `beforeEnd`);
 
-const tripEventList = tripEventSection.querySelector(`.trip-events__list`);
-
-for (let i = 0; i < POINT_COUNT; i++) {
-  render(tripEventList, createTripEventsItemTemplate(), `beforeEnd`);
+if (DAY_COUNT > 0) {
+  render(tripEventSection, creatDayListTemplate(), `beforeEnd`);
 }
