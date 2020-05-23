a5c764556cc8c447d6ae8138b028c6e8d019078c
Olya <osokolova1@gmail.com>
2020-05-23 17:09:59 +0300

    Правка отрисовки по дням

--- src/controllers/trip-controller.js
+++ src/controllers/trip-controller.js
@@ -27,17 +27,6 @@ export default class TripController {
     this._eventsModel.setFilterChangeHandler(this._onFilterChange);
   }
   render() {
-    // const events = this._eventsModel.getEvents();
-    /* if (DAY_COUNT > 0) {
-      for (let i = 0; i < DAY_COUNT; i++) {
-        const TripDaysItemElement = new TripDaysItem(i);
-        render(this._container, TripDaysItemElement, RenderPosition.BEFOREEND);
-        const tripDaysBlock = TripDaysItemElement.getTripDaysBlock();
-        // const newEvents = renderDaysEvents(tripDaysBlock, events.slice(i * POINT_COUNT, (i + 1) * POINT_COUNT), this._onDataChange, this._onViewChange);
-        const newEvents = renderDaysEvents(tripDaysBlock, events, this._onDataChange, this._onViewChange);
-        this._showedEventControllers = this._showedEventControllers.concat(newEvents);
-      }
-    }*/
     let events = this._eventsModel.getEvents();
 
     let eventsForDay = events.filter((event) => event.timeEvent.start.getDate() === events[0].timeEvent.start.getDate());
@@ -45,44 +34,15 @@ export default class TripController {
     let eventsShown = 0;
 
     while (eventsShown < events.length) {
-      console.log(eventsToRender);
-      console.log(eventsForDay);
+      eventsShown = eventsShown + eventsForDay.length;
       const TripDaysItemElement = new TripDaysItem(eventsToRender[0]);
       render(this._container, TripDaysItemElement, RenderPosition.BEFOREEND);
       const tripDaysBlock = TripDaysItemElement.getTripDaysBlock();
       const newEvents = renderDaysEvents(tripDaysBlock, eventsForDay, this._onDataChange, this._onViewChange);
       this._showedEventControllers = this._showedEventControllers.concat(newEvents);
       eventsToRender = eventsToRender.slice(eventsForDay.length);
-      console.log(eventsToRender);
       eventsForDay = eventsToRender.filter((event) => event.timeEvent.start.getDate() === eventsToRender[0].timeEvent.start.getDate());
-      console.log(eventsForDay);
     }
-
-    // const newEvents = renderDaysEvents(tripDaysBlock, events.slice(i * POINT_COUNT, (i + 1) * POINT_COUNT), this._onDataChange, this._onViewChange);
-
-
-
-    /*for (events; events.length > 0; events = events.slice(0, 5)) {
-      const eventsForDay = events.filter((event) => event.timeEvent.start === events[0].timeEvent.start);
-      const TripDaysItemElement = new TripDaysItem(events[0]);
-      render(this._container, TripDaysItemElement, RenderPosition.BEFOREEND);
-      const tripDaysBlock = TripDaysItemElement.getTripDaysBlock();
-      const newEvents = renderDaysEvents(tripDaysBlock, eventsForDay, this._onDataChange, this._onViewChange);
-      this._showedEventControllers = this._showedEventControllers.concat(newEvents);
-    }*/
-
-      //const eventsForDay = events.filter((event) => event.timeEvent.start === events[0].timeEvent.start);
-      //events.filter((event) => event.timeEvent.start === events[0].timeEvent.start).length)
-      //events = [].concat(events.slice(0, eventsForDay.length));
-
-      /*const TripDaysItemElement = new TripDaysItem(events[0]);
-      render(this._container, TripDaysItemElement, RenderPosition.BEFOREEND);
-      const tripDaysBlock = TripDaysItemElement.getTripDaysBlock();
-      const newEvents = renderDaysEvents(tripDaysBlock, eventsForDay, this._onDataChange, this._onViewChange);
-      this._showedEventControllers = this._showedEventControllers.concat(newEvents); */
-
-
-
   }
 
   _renderEvents(events) {
--- src/mock/time.js
+++ src/mock/time.js
@@ -4,7 +4,7 @@ import {getRandomIntegerNumber} from "./event";
 const getRandomDate = () => {
   const targetDate = new Date();
   const sign = Math.random() > 0.5 ? 1 : -1;
-  const diffValue = sign * getRandomIntegerNumber(0, 2);
+  const diffValue = sign * getRandomIntegerNumber(0, 4);
   targetDate.setDate(targetDate.getDate() + diffValue);
   return targetDate;
 };
